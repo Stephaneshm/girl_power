13d607918c49f6ebe8394a6d7ee907e33e3b33ea
SHM <stephane@delta-consulting.fr>
2020-04-27 14:28:00 +0200

    version 1.0 sans commentaire
    Signed-off-by: SHM <stephane@delta-consulting.fr>

--- girl_power.ino
+++ girl_power.ino
@@ -3,119 +3,55 @@
 #include <SoftwareSerial.h>
 #include <JQ6500_Serial.h>
 
-#define GES_REACTION_TIME    150       // You can adjust the reaction time according to the actual circumstance.
-#define GES_ENTRY_TIME      800       // When you want to recognize the Forward/Backward gestures, your gestures' reaction time must less than GES_ENTRY_TIME(0.8s). 
-#define GES_QUIT_TIME     500
+#define GES_REACTION_TIME    800       // You can adjust the reaction time according to the actual circumstance.
+//#define GES_ENTRY_TIME      800       // When you want to recognize the Forward/Backward gestures, your gestures' reaction time must less than GES_ENTRY_TIME(0.8s). 
+//#define GES_QUIT_TIME     250
+
+#define interruptPin  2
+
+unsigned long previousMillis = 0 ;
+unsigned long interval = 800L;       
 
 JQ6500_Serial mp3(8,9);
 
 uint8_t error = 0;
+uint8_t data = 0, data1 = 0;
 
 void Gesture(){
-  uint8_t data = 0, data1 = 0;
-  error = paj7620ReadReg(0x43, 1, &data);       // Read Bank_0_Reg_0x43/0x44 for gesture result.
+    
+  error = paj7620ReadReg(0x43, 1, &data);    
+//  Serial.println(data);
   if (!error) 
   {
-    switch (data)                   // When different gestures be detected, the variable 'data' will be set to different values by paj7620ReadReg(0x43, 1, &data).
-    {
-      case GES_RIGHT_FLAG:
-        delay(GES_ENTRY_TIME);
-        paj7620ReadReg(0x43, 1, &data);
-        if(data == GES_FORWARD_FLAG) 
-        {
-          Serial.println("Forward");
-          delay(GES_QUIT_TIME);
-        }
-        else if(data == GES_BACKWARD_FLAG) 
-        {
-          Serial.println("Backward");
-          delay(GES_QUIT_TIME);
-        }
-        else
-        {
-          Serial.println("Right");
-          mp3.playFileByIndexNumber(2);
-        }          
-        break;
-      case GES_LEFT_FLAG: 
-        delay(GES_ENTRY_TIME);
-        paj7620ReadReg(0x43, 1, &data);
-        if(data == GES_FORWARD_FLAG) 
-        {
-          Serial.println("Forward");
-          delay(GES_QUIT_TIME);
-        }
-        else if(data == GES_BACKWARD_FLAG) 
-        {
-          Serial.println("Backward");
-          delay(GES_QUIT_TIME);
-        }
-        else
-        {
-          Serial.println("Left");
-          mp3.playFileByIndexNumber(3);
-        }          
-        break;
-      case GES_UP_FLAG:
-        delay(GES_ENTRY_TIME);
-        paj7620ReadReg(0x43, 1, &data);
-        if(data == GES_FORWARD_FLAG) 
-        {
-          Serial.println("Forward");
-          delay(GES_QUIT_TIME);
-        }
-        else if(data == GES_BACKWARD_FLAG) 
-        {
-          Serial.println("Backward");
-          delay(GES_QUIT_TIME);
-        }
-        else
-        {
-          Serial.println("Up");
-        }          
-        break;
-      case GES_DOWN_FLAG:
-        delay(GES_ENTRY_TIME);
-        paj7620ReadReg(0x43, 1, &data);
-        if(data == GES_FORWARD_FLAG) 
-        {
-          Serial.println("Forward");
-          delay(GES_QUIT_TIME);
-        }
-        else if(data == GES_BACKWARD_FLAG) 
-        {
-          Serial.println("Backward");
-          delay(GES_QUIT_TIME);
-        }
-        else
-        {
-          Serial.println("Down");
-        }          
-        break;
-      case GES_FORWARD_FLAG:
+  if      (data==GES_CLOCKWISE_FLAG) { Serial.println("Clockwise");}
+  else if (data==GES_COUNT_CLOCKWISE_FLAG) {Serial.println("anti-clockwise");}
+  else if (data==GES_RIGHT_FLAG) {
+        Serial.println("Right");
+        mp3.playFileByIndexNumber(4);
+      }
+  else if (data==GES_LEFT_FLAG) {
+        Serial.println("Left");
+        mp3.playFileByIndexNumber(5);
+      }
+  else if (data==GES_UP_FLAG) { 
+        Serial.println("Up");
+        mp3.playFileByIndexNumber(2);
+      }
+  else if (data==GES_DOWN_FLAG) {
+        Serial.println("Down");
+        mp3.playFileByIndexNumber(3);
+      }
+  else if (data==GES_FORWARD_FLAG) {
         Serial.println("Forward");
-        delay(GES_QUIT_TIME);
-        break;
-      case GES_BACKWARD_FLAG:     
+        mp3.playFileByIndexNumber(6);
+      }
+  else if (data==GES_BACKWARD_FLAG) {
         Serial.println("Backward");
-        delay(GES_QUIT_TIME);
-        break;
-      case GES_CLOCKWISE_FLAG:
-        Serial.println("Clockwise");
-        break;
-      case GES_COUNT_CLOCKWISE_FLAG:
-        Serial.println("anti-clockwise");
-        break;  
-      default:
-        paj7620ReadReg(0x44, 1, &data1);
-        if (data1 == GES_WAVE_FLAG) 
-        {
-          Serial.println("wave");
-        }
-        break;
-    }
-  }
-  delay(100);
+        mp3.playFileByIndexNumber(7);
+      }
+  delay(GES_REACTION_TIME);
+   
+ }
   
 }
 
@@ -126,26 +62,22 @@ void setup() {
   Serial.begin(115200);
   mp3.begin(9600);
   mp3.reset();
-  //statusAndHelpOutput();
-
   mp3.setVolume(30);
   mp3.setEqualizer(MP3_EQ_NORMAL);
   mp3.playFileByIndexNumber(1);
-
   error = paj7620Init();      // initialize Paj7620 registers
-  if (error) 
-  {
-    Serial.print("INIT ERROR,CODE:"); Serial.println(error);
-  }
-  else
-  {
-    Serial.println("INIT OK");
-  }
+  if (error)   {Serial.print("INIT ERROR,CODE:"); Serial.println(error); }
+  else         {Serial.println("INIT OK"); }
 
-
+  
 }
 
+
 void loop() {
-  Gesture();
+  
+  if ( millis() - previousMillis >= interval) { 
+        Gesture(); 
+        previousMillis = millis();               
+  }
 
 }
